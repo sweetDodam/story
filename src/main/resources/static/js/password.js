e9ebea35ef99c00f76d213264a1b1617abfb5932
ch-you <ypuoi2003@hanmail.net>
2019-01-28 15:28:50 +0900

    비밀번호 변경화면, 로딩이미지, 메인 이미지 추가

---
+++ src/main/resources/static/js/password.js
@@ -0,0 +1,45 @@
+var passwordForm = {
+    init : function () {
+        var _this = this;
+        console.log("passwordForm init");
+    },
+    save : function () {
+        //필수체크 검사
+        if(!common.formChk($("#form"))){
+            return;
+        }
+
+        var password = $("#password").val();
+        var password2 = $("#password2").val();
+
+
+        if(password != password2){
+            alert("비밀번호가 일치하지 않습니다.");
+            $("#password2").focus();
+            return;
+        }
+
+        var form = $("#form")[0];
+        var formData = new FormData(form);
+
+        url = "/services/user/passwordUpdate";
+        txt = "수정";
+
+        $.ajax({
+            type: 'POST',
+            url: url,
+            processData: false,
+            contentType: false,
+            cache : false,
+            data : formData
+        }).done(function(rs) {
+            alert(rs.msg);
+            document.location.href = "/";
+        }).fail(function (error) {
+            console.debug(txt + "실패");
+            alert(error);
+        });
+    }
+};
+
+passwordForm.init();
